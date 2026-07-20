a765ea58b8f18722484cbf610dce1cd5b7e1ee12
Rohit Pandey <vvmrohit10@gmail.com>
2026-07-20 23:54:47 +0530

    Fix checkov issues

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,12 +8,47 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_iam_role" "app_role" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "app_profile" {
+  name = "${var.project_name}-ec2-profile"
+  role = aws_iam_role.app_role.name
+}
+
 resource "aws_instance" "app" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  ebs_optimized               = true
+  monitoring                  = true
+  iam_instance_profile        = aws_iam_instance_profile.app_profile.name
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  root_block_device {
+    encrypted = true
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
 
   user_data = <<-EOF
 #!/bin/bash
