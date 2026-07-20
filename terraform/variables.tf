35c72cd8d2174702fe825153144dcfb5ace1bb52
Rohit Pandey <vvmrohit10@gmail.com>
2026-07-21 00:26:55 +0530

    Revert project_name back to hw2-staging after successful state import

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   type        = string
   description = "The name of the project"
-  default     = "hw2-staging-v2"
+  default     = "hw2-staging"
 }
 
 variable "vpc_cidr" {
